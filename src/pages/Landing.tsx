4c87a4591ce9710b910cea8cbf2bcb86a0df7c66
doniyor <qambaralievd@gmail.com>
2025-07-26 03:03:55 +0500

    fix: swiper

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import { DeadlinesBlock } from './Blocks/Deadlines/Deadlines';
 import { MainBlock } from './Blocks/Main/Main';
 import { ReasonsBlock } from './Blocks/Reasons/ReasonsBlock';
-import { ResultsBlock } from './Blocks/Results/ResultsBlocl';
+import { ResultsBlock } from './Blocks/Results/ResultsBlock';
 
 const Landing = () => (
   <>
